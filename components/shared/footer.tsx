dcd4a35b1ee1cbea84335541d42df2fabe62d118
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-09-27 17:15:34 +0600

    update footer ui

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import ContainerWrapper from "@/components/common/ContainerWrapper";
-
 import {
   FaYoutube,
   FaXTwitter,
@@ -11,39 +10,37 @@ import {
 import { FaApple } from "react-icons/fa";
 
 const socialLinks = [
-  { name: "YouTube", href: "#", icon: <FaYoutube className="w-6 h-6" /> },
-  { name: "X (Twitter)", href: "#", icon: <FaXTwitter className="w-6 h-6" /> },
-  { name: "Facebook", href: "#", icon: <FaFacebookF className="w-6 h-6" /> },
-  { name: "Instagram", href: "#", icon: <FaInstagram className="w-6 h-6" /> },
+  { name: "YouTube", href: "#", icon: <FaYoutube className="w-5 h-5" /> },
+  { name: "X (Twitter)", href: "#", icon: <FaXTwitter className="w-5 h-5" /> },
+  { name: "Facebook", href: "#", icon: <FaFacebookF className="w-5 h-5" /> },
+  { name: "Instagram", href: "#", icon: <FaInstagram className="w-5 h-5" /> },
 ];
 
 export default function Footer() {
   return (
-    <footer className="bg-[#0F3B34] text-white">
+    <footer className="relative overflow-hidden bg-[#0F3B34] text-white">
+      <div
+        className="absolute bottom-0 right-0 h-full w-full bg-no-repeat bg-right bg-cover xl:bg-contain pointer-events-none"
+        style={{ backgroundImage: "url('/mask-group.png')" }}
+      ></div>
       <ContainerWrapper>
-        {/* Top Row */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-center pt-5">
-          {/* Logo Section */}
+        <div className="grid grid-cols-1 lg:grid-cols-[0.7fr_1fr_1fr] gap-12 items-center py-10">
           <div>
             <Image
               src="/logo-light.svg"
               alt="ScapeSync Logo"
-              width={120}
+              width={160}
               height={80}
               className="object-contain"
             />
           </div>
 
-          {/* Description Section */}
-          <div>
-            <p className="leading-relaxed text-sm text-[#CFD8D6]">
-              Your all-in-one platform for job scheduling, employee management,
-              and client service built to keep your business running smoothly
-              from anywhere.
-            </p>
-          </div>
+          <p className="leading-relaxed text-xs w-[360px] text-[#CFD8D6]">
+            Your all-in-one platform for job scheduling, employee management,
+            and client service built to keep your business running smoothly from
+            anywhere.
+          </p>
 
-          {/* App Store Buttons */}
           <div className="flex flex-wrap flex-row gap-4 lg:justify-end">
             <button className="cursor-pointer px-4 py-2 rounded-md border border-[#C7E6C5] flex items-center gap-3">
               <FaApple className="size-7" />
@@ -71,13 +68,12 @@ export default function Footer() {
           </div>
         </div>
 
-        {/* Social Media Icons */}
-        <div className="flex justify-start mt-10 mb-6 gap-6">
+        <div className="flex justify-start lg:my-2 gap-6">
           {socialLinks.map((social) => (
             <Link
               key={social.name}
               href={social.href}
-              className="text-emerald-200 hover:text-white transition-colors duration-200"
+              className="text-white"
               aria-label={social.name}
             >
               {social.icon}
@@ -86,9 +82,8 @@ export default function Footer() {
         </div>
       </ContainerWrapper>
 
-      {/* Bottom */}
-      <div className="mt-8 border-t border-white/30">
-        <div className="container mx-auto py-5 px-5 md:px-10 lg:px-14">
+      <div className="mt-2 lg:mt-4 border-t border-white/30">
+        <div className="container mx-auto py-2 px-5 md:px-10 lg:px-14">
           <p className="text-sm text-[#CFD8D6]">
             © 2021-2025, ScapeSync. All Rights Reserved.
           </p>
